d2d4215c3acb22265d31cc3a74ad8a50ae5223ee
kalmod <kalmod@live.com>
2023-04-10 19:18:19 -0400

    Finalized 3d model of Calculator

--- README.md
+++ README.md
@@ -11,11 +11,15 @@ This is the Calculator Project from the Odin Project.
     - May need to use z-index on Screen if I try to establish depth.
     - Set Font for Screen.
     - Character limit??
+    - Added a screen for history. Maybe look into adjusting the font size based on length for both? [Possible Reference](https://gabriellazcano.com/blog/how-to-auto-adjust-font-size-to-fit-div/)
   - Create Calculator Buttons
     - Buttons should animate on click.
     - Numbers, Operations, Equals/Enter, Clear
     - Generate buttons automatically with JS.
       - translation distnace is based off of half of height & width. Must grab both using js and apply.
+      - Had to set top of button to a top 0 so it animates correctly.
+      - width of left & right = height of container
+      - width of front & back = width of container
 
 2. Configure CSS Behavior.
   - Create the following functions:
@@ -30,3 +34,4 @@ This is the Calculator Project from the Odin Project.
     - Calling an operand doesn't erase the previous entered number. Remains until a user inputs a new one.
       If they press enter without using a new number, uses what ever is currently displayed.
   - Decimal numbers.
+  - Add button to control position of calculator.
--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 // const keypad = document.querySelector('#keypad');
 
-// keypadClassNames = ['clear','div', 'multi', 'min', 'num','plus','zero0','deci','equals'];
-// keypadClassSymbols = {'clear':'C','div':'/', 'multi':'*', 'min':'-', 'num':'','plus':'+','zero0':'0','deci':'.','equals':'=',
-//                         'n1':'1','n2':'2','n3':'3','n4':'4','n5':'5','n6':'6','n7':'7','n8':'8','n9':'9'};
-
-keypadClassNames = [,'div', 'multi', 'min','plus','zero0','deci','equals'];
-keypadClassSymbols = {'div':'/', 'multi':'*', 'min':'-', 'plus':'+','zero0':'0','deci':'.','equals':'=',
+const keypadClassNames = ['clear','div', 'multi', 'min', 'num','plus','zero0','deci','equals'];
+const keypadClassSymbols = {'clear':'C','div':'/', 'multi':'*', 'min':'-', 'num':'','plus':'+','zero0':'0','deci':'.','equals':'=',
                         'n1':'1','n2':'2','n3':'3','n4':'4','n5':'5','n6':'6','n7':'7','n8':'8','n9':'9'};
+const cubeFaces = ['front','right','back','left','top'];
+const screen = document.querySelector('.screen');
+const screenHistory = document.querySelector('.screen-history');
+let operand = '';
+let mathEquation = [];
+
+const add = function(a,b){
+    return parseInt(a) + parseInt(b);
+}
+const subtract = function(a,b){
+    return parseInt(a) - parseInt(b);
+}
+const multiply = function(a,b){
+    return parseInt(a) * parseInt(b);
+}
+const divide = function(a,b){
+    return parseInt(a) / parseInt(b);
+}
+
+
+populateKeypad();
+populateNumbers();
+
+for (let keypad in keypadClassSymbols){
+    create3dButton(keypad);
+}
+
+
 
 
 function populateKeypad(){
@@ -29,23 +53,80 @@ function populateNumbers(){
 }
 
 
-
 function createButton(parentElement,className){
     const newButton = document.createElement('div');
     newButton.classList.add(className);
     newButton.setAttribute('style',`grid-area:${className}`);
-    newButton.textContent = keypadClassSymbols[className];
+    // newButton.textContent = keypadClassSymbols[className];
     parentElement.appendChild(newButton);
-
 }
 
-populateKeypad();
-populateNumbers();
 
-const numPadButton = document.querySelector('.buttonCube');
-numPadButton.addEventListener('click', function(){
-    this.classList.add('click-animation');
-    setTimeout(() => {
-        this.classList.remove('click-animation');
-    }, 250)
+
+
+
+
+function create3dButton(selectedClass){
+    const className = `.${selectedClass}`;
+    const testbutton = document.querySelector(`${className}`);
+    const buttonElement = document.createElement('div');
+    cubeFaces.forEach((face)=>{createButton(buttonElement,face)})
+    buttonElement.classList.add('buttonCube');
+
+
+    testbutton.appendChild(buttonElement);
+    
+    document.querySelector(`${className} > .buttonCube > .top`).style.width = `${testbutton.clientWidth}px`;
+    document.querySelector(`${className} > .buttonCube > .top`).style.height = `${testbutton.clientHeight}px`;
+    document.querySelector(`${className} > .buttonCube > .top`).textContent = keypadClassSymbols[selectedClass];
+    // document.querySelector(`${className} > .buttonCube > .top`).style.setProperty('--ztranslation-distance',`${Math.floor((document.querySelector(`${className} > .buttonCube > .front`)).clientHeight/2)}px`)
+    
+    document.querySelector(`${className} > .buttonCube > .front`).style.setProperty('--ztranslation-distance-h',`${Math.floor(testbutton.clientHeight/2)}px`);
+    document.querySelector(`${className} > .buttonCube > .front`).style.setProperty('--cubeWidth',`${testbutton.clientWidth}px`);
+
+    document.querySelector(`${className} > .buttonCube > .back`).style.setProperty('--ztranslation-distance-h',`${Math.floor(testbutton.clientHeight/2)}px`);
+    document.querySelector(`${className} > .buttonCube > .back`).style.setProperty('--cubeWidth',`${testbutton.clientWidth}px`);
+
+    document.querySelector(`${className} > .buttonCube > .right`).style.setProperty('--ztranslation-distance-w',`${Math.floor(testbutton.clientWidth/2)}px`);
+    document.querySelector(`${className} > .buttonCube > .right`).style.setProperty('--cubeWidth',`${testbutton.clientHeight}px`);
+
+    document.querySelector(`${className} > .buttonCube > .left`).style.setProperty('--ztranslation-distance-w',`${Math.floor(testbutton.clientWidth/2)}px`);
+    document.querySelector(`${className} > .buttonCube > .left`).style.setProperty('--cubeWidth',`${testbutton.clientHeight}px`);
+}
+
+
+const numPadButtons = document.querySelectorAll('.buttonCube');
+
+numPadButtons.forEach((numPadButton)=>{
+    numPadButton.addEventListener('click', function(){
+        this.classList.add('click-animation');
+        setTimeout(()=>{
+            this.classList.remove('click-animation');
+        },250);
+        // console.log(this.textContent, this.classList, this.parentElement.classList);
+        console.log(this.textContent);
+        if (this.textContent === 'C') {
+            operand = '';
+            mathEquation = [];
+            screenHistory.textContent = '';
+        }
+        else if (this.textContent === '='){
+            mathEquation.push(operand);
+            console.log(mathEquation.join(''), mathEquation);
+            screenHistory.textContent= mathEquation.join('');
+            operand = '';
+            mathEquation = [];
+        } else if (['+','/','*','-'].includes(this.textContent)){
+            if (['+','/','*','-'].includes(mathEquation[mathEquation.length-1]) && operand === ''){
+                mathEquation[mathEquation.length-1] = this.textContent;
+            } else if (operand.length > 0){
+                mathEquation.push(...[operand,this.textContent]);
+                operand = '';
+            }
+        } else {
+            operand+=this.textContent;
+        };
+        screen.textContent = `${mathEquation.join(' ')} ${operand}`;
+    })
+
 })
